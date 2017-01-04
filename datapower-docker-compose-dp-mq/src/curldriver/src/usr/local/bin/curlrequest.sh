766edbdf25f9eb1e7a69d61f2693a8f0811df92b
jefurbee <jefurbee@us.ibm.com>
2017-01-04 10:36:13 -0500

    modified markup and curldriver script and deleted a temp file

--- datapower-docker-compose-dp-mq/src/curldriver/src/usr/local/bin/curlrequest.sh
+++ datapower-docker-compose-dp-mq/src/curldriver/src/usr/local/bin/curlrequest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sleep 30
 
-for (( i=1; i <= 100; i++ ))
+for (( i=1; i <= 5; i++ ))
   do
    echo "sending message $i"
      curl --silent --insecure --form "project=@/usr/local/bin/grow-soapui-project.xml" --form "suite=sendRequestGrow" http://soapui:3000 || echo curl error $?
