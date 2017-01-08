d51f0efb911610dc448a10a2dd99ea6c4cc38c4a
jefurbee <jefurbee@us.ibm.com>
2017-01-08 15:33:38 -0500

    updated markup based on comments from others

--- datapower-docker-compose-dp-mq/src/curldriver/src/usr/local/bin/curlrequest.sh
+++ datapower-docker-compose-dp-mq/src/curldriver/src/usr/local/bin/curlrequest.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
-sleep 30
+# set env vars to modify operation
+# TIMEOUT=<time in seconds> for amount of time to continue to retry
+# CONTINUOUS=true to continuously test, otherwise exits immediately on success
 
-for (( i=1; i <= 5; i++ ))
-  do
-   echo "sending message $i"
-     curl --silent --insecure --form "project=@/usr/local/bin/grow-soapui-project.xml" --form "suite=sendRequestGrow" http://soapui:3000 || echo curl error $?
-   echo "response $i received"
+if [ "$TIMEOUT" -a "$TIMEOUT" -gt 0 ]
+then
+  STOPTIME=$(( $(date +%s) + $TIMEOUT ))
+else
+  STOPTIME=9999999999
+fi
+
+RC=1
+
+while [ $(date +%s) -lt $STOPTIME ]
+do
+echo "SENDING REQUEST"
+  if curl --silent --insecure --form "project=@/usr/local/bin/grow-soapui-project.xml" --form "suite=sendRequestGrow" http://soapui:3000 | grep "200 OK"
+  then
+    RC=0
+    if [ "$CONTINUOUS" != "true" ]
+    then
+      echo "SUCCESS! Exiting"
+      exit $RC
+    fi
+  else
+    echo curl error $?
+  fi
   sleep 5
-  done
+done
